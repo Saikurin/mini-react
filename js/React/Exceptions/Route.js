f4c2fefefc8d28453def511917488104cb362a1e
Nashad <nashad@MacBook-Pro-de-Nashad-2.home>
2021-02-09 23:14:33 +0100

    init project

--- js/React/Exceptions/Route.js
+++
@@ -1,9 +0,0 @@
-export const RouteNotFoundException = function (route) {
-    const error = new Error(`Route ${route} doesn't found`);
-
-    if (Error.captureStackTrace){
-        Error.captureStackTrace(error, RouteNotFoundException);
-    }
-
-    return error;
-}
