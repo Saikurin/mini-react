11a4d25162d9a8a2592691dfe01262e48d154a65
Théo SIKLI <theo@theo-sikli.fr>
2021-02-09 23:47:23 +0100

    add router with differents route for project test + utils function use for the project

---
+++ src/React/Rutils.js
@@ -0,0 +1,113 @@
+String.prototype.snake_case = function() {
+  var input = this;
+  return input
+    .toLowerCase()
+    .split(" ")
+    .join("_");
+};
+
+Object.prototype.isClass = () => {
+  return (
+    typeof this === "function" &&
+    /^class\s/.test(Function.prototype.toString.call(this))
+  );
+};
+
+Number.prototype.createScore = function(nbr) {
+  if (type_check(nbr, 'number')) return this + nbr;
+  else return this + 1;
+};
+
+
+
+
+
+
+export function isStateLessComponent(element) {
+  return !element.isClass() && typeof element === "function";
+}
+
+function type_check_v1(data, type) {
+  switch (typeof data) {
+    case "number":
+    case "string":
+    case "boolean":
+    case "undefined":
+    case "function":
+      return type === typeof data;
+    case "object":
+      switch (type) {
+        case "null":
+          return data === null;
+        case "array":
+          return Array.isArray(data);
+        default:
+          return data !== null && !Array.isArray(data);
+      }
+  }
+
+  return false;
+}
+
+function type_check_v2(data, conf) {
+  for (let key of Object.keys(conf)) {
+    switch (key) {
+      case "type":
+        if (!type_check_v1(data, conf[key])) return false;
+        break;
+      case "value":
+        if (JSON.stringify(data) !== JSON.stringify(conf[key])) return false;
+        break;
+      case "enum":
+        let valid = false;
+        for (let value of conf[key]) {
+          valid = type_check_v2(data, { value });
+          if (valid) break;
+        }
+        if (!valid) return false;
+    }
+  }
+
+  return true;
+}
+
+export function type_check(data, conf) {
+  for (let key of Object.keys(conf)) {
+    switch (key) {
+      case "type":
+      case "value":
+      case "enum":
+        let newConf = {};
+        newConf[key] = conf[key];
+        if (!type_check_v2(data, newConf))
+          throw new Error("Type properties error");
+        break;
+      case "properties":
+        for (let prop of Object.keys(conf[key])) {
+          if (data[prop] === undefined)
+            throw new Error("Type properties error");
+          if (!type_check(data[prop], conf[key][prop]))
+            throw new Error("Type properties error");
+        }
+        break;
+    }
+  }
+
+  return true;
+}
+
+export function prop_access(object, path) {
+  object = object || {};
+  if (!path) return object;
+  const pathArray = path.split(".");
+
+  for (let i = 0; i < pathArray.length; i++) {
+    object = object[pathArray[i]];
+    if (object === undefined) {
+      console.log(pathArray.slice(0, i + 1).join(".") + " not exist");
+      return null;
+    }
+  }
+
+  return object;
+}
---
+++ src/router.js
@@ -0,0 +1,12 @@
+import { Router, Route } from "./lib/react-routing.js";
+
+
+
+
+export var router = new Router("mainRouter", [
+  new Route("Clicker", "clicker", "/clicker", "link color-yellow")
+]);
+
+export var route = router.routes.filter(function(r) {
+  return r.getPath() === window.location.pathname;
+})[0];
